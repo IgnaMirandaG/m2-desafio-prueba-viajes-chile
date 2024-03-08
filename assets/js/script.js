36327fb24d507fd8ae9f8daf2cc693d2542a9b5a
Ignacia Miranda <ign.miranda.g@gmail.com>
2024-03-08 14:31:32 -0300

    Se agrega evento Alert en Summit al enviar formulario de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
+// CONST PARA FUNCIONAMINETO DE TOOLTIPS 
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+// EVENTO PARA CAPTURAR NOMBRE EN EL FORMULARIO DE CONTACTO PARA MOSTRAR UN MENSAJE ALERT
+
+$(function(){
+    $("#formContacto").on("submit", function(event){
+        event.preventDefault();
+        var nombre = $("#nombre").val();
+        var mensaje = `Tu mensaje fue enviado con éxito. Muchas gracias ${nombre} por contactarte con nosotros.`;
+        alert(mensaje);
+        this.reset();
+    })
+});
